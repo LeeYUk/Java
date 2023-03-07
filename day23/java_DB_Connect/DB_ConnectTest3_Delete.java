24f9a32a7bb3c35530af2f5c45641007bf3a6236
이용욱 <118726154+LeeYUk@users.noreply.github.com>
2023-03-07 17:58:12 +0900

    day23
    java랑 db를 연결하고
    insert, delete, update를 사용

---
+++ day23/java_DB_Connect/DB_ConnectTest3_Delete.java
@@ -0,0 +1,48 @@
+package java_DB_Connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class DB_ConnectTest3_Delete {
+
+	public static void main(String[] args) {
+		// 자바 연결 테스트
+		//1.오라클 11g와 연결 부품설정
+		//2.오라클 11g에 연결(java--oracle)
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver"); //오라클 11g와 드라이버 연결(커넥터 연결)
+			System.out.println("...커넥터 연결");
+			
+			String url="jdbc:oracle:thin:@localhost:1521:xe"; //오라클 11g에 연결(DB연결)
+			String user= "system"; //oracle user계정
+			String passward="oracle"; //oracle 계정 비밀번호
+			 Connection con = DriverManager.getConnection(url,user,passward);
+			 System.out.println("연결성공");
+			 
+			 
+			 //ipaddress ==>InetAddress;
+			 //String url="http://www.naver.com";
+			 //URL u =new URL(url);
+			 //자바는 String에 넣은 문자열을 특정한 객체로 인식하지 못함
+			 //SQL객체로 만들어줘야함
+			 
+			 String sql= "delete from HR.MEMBER2 where id='win2'"; //sql구문 입력후 저장
+			 PreparedStatement ps =con.prepareStatement(sql);//저장한 sql문을 sql객체로 전환
+			 System.out.println("sql객체 생성");
+			 
+			 int result=ps.executeUpdate(); //sql객체를  db로 업데이트 전송
+			 System.out.println(result);
+			 
+		} 
+		catch (Exception e) {
+			// 예외처리
+			System.out.println("오류발생");
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
---
+++ day23/java_DB_Connect/MemberDAO2.java
@@ -0,0 +1,129 @@
+package java_DB_Connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class MemberDAO2 {
+
+	public void delete(String id) {
+		
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver"); //오라클 11g와 드라이버 연결(커넥터 연결)
+		System.out.println("...커넥터 연결");
+		
+		String url="jdbc:oracle:thin:@localhost:1521:xe"; //오라클 11g에 연결(DB연결)
+		String user= "system"; //oracle user계정
+		String passward="oracle"; //oracle 계정 비밀번호
+		 Connection con = DriverManager.getConnection(url,user,passward);
+		 System.out.println("연결성공");
+		 
+		 
+		 //ipaddress ==>InetAddress;
+		 //String url="http://www.naver.com";
+		 //URL u =new URL(url);
+		 //자바는 String에 넣은 문자열을 특정한 객체로 인식하지 못함
+		 //SQL객체로 만들어줘야함
+		 
+		 
+		 
+		 String sql= "delete from HR.MEMBER2 where id= ?";//sql구문 입력후 저장
+		 PreparedStatement ps =con.prepareStatement(sql);//저장한 sql문을 sql객체로 전환
+		 ps.setString(1, id);
+		 System.out.println("sql객체 생성");
+		 
+		 int result=ps.executeUpdate(); //sql객체를  db로 업데이트 전송
+		 System.out.println(result);
+		 
+	} 
+	catch (Exception e) {
+		// 예외처리
+		System.out.println("오류발생");
+		e.printStackTrace();
+		}
+		
+	}
+	
+	public void update(String id, String tel) {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver"); //오라클 11g와 드라이버 연결(커넥터 연결)
+			System.out.println("...커넥터 연결");
+			
+			String url="jdbc:oracle:thin:@localhost:1521:xe"; //오라클 11g에 연결(DB연결)
+			String user= "system"; //oracle user계정
+			String passward="oracle"; //oracle 계정 비밀번호
+			 Connection con = DriverManager.getConnection(url,user,passward);
+			 System.out.println("연결성공");
+			 
+			 
+			 //ipaddress ==>InetAddress;
+			 //String url="http://www.naver.com";
+			 //URL u =new URL(url);
+			 //자바는 String에 넣은 문자열을 특정한 객체로 인식하지 못함
+			 //SQL객체로 만들어줘야함
+			 
+			 String sql= "update HR.MEMBER2 set tel=? where id=?";//sql구문 입력후 저장
+			 PreparedStatement ps =con.prepareStatement(sql);//저장한 sql문을 sql객체로 전환
+			 ps.setString(1, tel);
+			 ps.setString(2, id);
+			 System.out.println("sql객체 생성");
+			 
+			 int result=ps.executeUpdate(); //sql객체를  db로 업데이트 전송 (insert,update, delete문만 사용가능) sql문 실행결과가 int이기 때문
+			 System.out.println(result);
+			 
+		} 
+		catch (Exception e) {
+			// 예외처리
+			System.out.println("오류발생");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void insert(String id, String pw, String name, String tel) {
+		// 자바 연결 테스트
+		//1.오라클 11g와 연결 부품설정
+		//2.오라클 11g에 연결(java--oracle)
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver"); //오라클 11g와 드라이버 연결(커넥터 연결)
+			System.out.println("...커넥터 연결");
+			
+			String url="jdbc:oracle:thin:@localhost:1521:xe"; //오라클 11g에 연결(DB연결)
+			String user= "system"; //oracle user계정
+			String passward="oracle"; //oracle 계정 비밀번호
+			 Connection con = DriverManager.getConnection(url,user,passward);
+			 System.out.println("연결성공");
+			 
+			 
+			 //ipaddress ==>InetAddress;
+			 //String url="http://www.naver.com";
+			 //URL u =new URL(url);
+			 //자바는 String에 넣은 문자열을 특정한 객체로 인식하지 못함
+			 //SQL객체로 만들어줘야함
+			 
+			 
+			 
+			 String sql= "INSERT INTO HR.MEMBER2 VALUES(?,?,?,?)";//sql구문 입력후 저장
+			 PreparedStatement ps =con.prepareStatement(sql);//저장한 sql문을 sql객체로 전환
+			 ps.setString(1, id);
+			 ps.setString(2, pw);
+			 ps.setString(3, name);
+			 ps.setString(4, tel);
+			 System.out.println("sql객체 생성");
+			 
+			 int result=ps.executeUpdate(); //sql객체를  db로 업데이트 전송
+			 System.out.println(result);
+			 
+		} 
+		catch (Exception e) {
+			// 예외처리
+			System.out.println("오류발생");
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
